d30a0e1a2e3b38b2fe553993d15183fc530ca7b4
Jason Lavoie <jlavoie@bowdoin.edu>
2020-08-26 15:25:59 -0400

    refactor syntax to mirror Test::Script syntax

--- tests/HookUtils.pm
+++ tests/HookUtils.pm
@@ -11,6 +11,8 @@ our @EXPORT_OK = qw(test_script);
 my $DIR         = tempdir(CLEANUP => 1);
 my $ANSI_escape = qr/\e\[[0-9;]*m/;
 my $S;
+my $output;
+my $stderr;
 
 # run a hook script with varied input
 # test exit value and all combinations of --fix/--nofix and --color/nocolor
@@ -21,65 +23,65 @@ sub test_script {
 
   script_compiles($S);
   script_runs([ $S, '/tmp/nonexistent', '/dev/null' ], 'multiple file args');
-  script_runs($S, { exit => 1 }, 'no args');
-  run_fails('invalid args', '', ['--not-implemeted']);
+  run_fail('no args');
+  run_fail('invalid args', '', args => ['--not-implemeted']);
 
   # check valid entries individually
   for (@$valid) {
     my $c = 'valid';
-    my ($o, $e) = run($c, $_);
-    is($o, $_, "$c no change");
-    unlike($e, $ANSI_escape, "$c no color");
+    run($c, $_);
+    output_is($_, "$c no change");
+    stderr_unlike($ANSI_escape, "$c no color");
   }
 
   # check valid entries together
   {
     my $c  = 'valids';
     my $in = join("\n", @$valid);
-    my ($o, $e) = run($c, $in);
-    is($o, $in, "$c no change");
-    unlike($e, $ANSI_escape, "$c no color");
+    run($c, $in);
+    output_is($in, "$c no change");
+    stderr_unlike($ANSI_escape, "$c no color");
   }
 
   # check invalid entries individually
   for (@$invalid) {
-    my ($c, $o, $e);
+    my $c;
 
     # normal (nofix is default)
     $c = 'invalid';
-    ($o, $e) = run_fails($c, $_->[0], ['--color']);
-    is($o, $_->[0], "$c no change");
-    like($e, $ANSI_escape, "$c color");
+    run_fail($c, $_->[0], args => ['--color']);
+    output_is($_->[0], "$c no change");
+    stderr_like($ANSI_escape, "$c color");
 
     # normal with explicit nofix
     $c = 'invalid nofix';
-    ($o, $e) = run_fails($c, $_->[0], [ '--color', '--nofix' ]);
-    is($o, $_->[0], "$c no change");
-    like($e, $ANSI_escape, "$c color");
+    run_fail($c, $_->[0], args => [ '--color', '--nofix' ]);
+    output_is($_->[0], "$c no change");
+    stderr_like($ANSI_escape, "$c color");
 
     # fix
     $c = 'invalid fix';
-    ($o, $e) = run_fails($c, $_->[0], [ '--color', '--fix' ]);
-    is($o, $_->[1], "$c fixed");
-    like($e, $ANSI_escape, "$c color");
+    run_fail($c, $_->[0], args => [ '--color', '--fix' ]);
+    output_is($_->[1], "$c fixed");
+    stderr_like($ANSI_escape, "$c color");
 
     # without terminal
     $c = 'invalid noterm';
-    ($o, $e) = run_fails($c, $_->[0], ['--nocolor']);
-    is($o, $_->[0], "$c no change");
-    unlike($e, $ANSI_escape, "$c no color");
+    run_fail($c, $_->[0], args => ['--nocolor']);
+    output_is($_->[0], "$c no change");
+    stderr_unlike($ANSI_escape, "$c no color");
 
     # without terminal and explicit nofix
     $c = 'invalid noterm nofix';
-    ($o, $e) = run_fails($c, $_->[0], [ '--nocolor', '--nofix' ]);
-    is($o, $_->[0], "$c no change");
-    unlike($e, $ANSI_escape, "$c no color");
+    run_fail($c, $_->[0], args => [ '--nocolor', '--nofix' ]);
+    output_is($_->[0], "$c no change");
+    stderr_unlike($ANSI_escape, "$c no color");
 
     # fix without terminal
     $c = 'invalid noterm fix';
-    ($o, $e) = run_fails($c, $_->[0], [ '--nocolor', '--fix' ]);
-    is($o, $_->[1], "$c fixed");
-    unlike($e, $ANSI_escape, "$c no color");
+    run_fail($c, $_->[0], args => [ '--nocolor', '--fix' ]);
+    output_is($_->[1], "$c fixed");
+    stderr_unlike($ANSI_escape, "$c no color");
 
   }
 
@@ -88,33 +90,51 @@ sub test_script {
     my $c   = 'valids fix';
     my $in  = join("\n", map { $_->[0] } @$invalid);
     my $out = join("\n", map { $_->[1] } @$invalid);
-    my ($o, $e) = run_fails($c, $in, [ '--color', '--fix' ]);
-    is($o, $out, "$c fixed");
-    like($e, $ANSI_escape, "$c color");
+    run_fail($c, $in, args => [ '--color', '--fix' ]);
+    output_is($out, "$c fixed");
+    stderr_like($ANSI_escape, "$c color");
   }
+
 }
 
 # helper function to run script and output (possibly modified) input file and stderr
 sub run {
   my $check = shift;
   my $input = shift;
-  my $args  = shift || [];
-  my $exit  = shift || 0;
-  my $e;
+  my %opt = (args => [],
+             exit => 0,
+             @_
+            );
 
   my ($ifh, $ifn) = tempfile(DIR => $DIR);
   print $ifh $input;
 
-  script_runs([ $S, @$args, $ifn ], { exit => $exit, stderr => \$e }, $check);
+  script_runs([ $S, @{ $opt{'args'} }, $ifn ],
+              { exit => $opt{'exit'}, stderr => \$stderr },
+              $check
+             );
 
   open F, "<$ifn";
-  my $o = do { local $/; <F> };
-
-  return ($o, $e);
+  my $o = $output = do { local $/; <F> };
 }
 
-sub run_fails {
-  run(@_, 1);
+sub run_fail {
+  run(@_, exit => 1);
+}
+
+sub output_is {
+  my ($match, $check) = @_;
+  is($output, $match, $check);
+}
+
+sub stderr_like {
+  my ($match, $check) = @_;
+  like($stderr, $match, $check);
+}
+
+sub stderr_unlike {
+  my ($match, $check) = @_;
+  unlike($stderr, $match, $check);
 }
 
 1;
